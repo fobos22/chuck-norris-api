7c51aeda139c8756c550bbd7ec3022741006e5e0
seiyadev <axelchavezuwu@gmail.com>
2022-11-19 18:18:23 -0600

    second commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,5 @@
 import { createPool } from "mysql2/promise";
 
-const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "n0m3l0",
-  database: "chuck",
-  port: 3306,
-});
+const pool = createPool('mysql://uloeu5msem1i842csl33:pscale_pw_B0dXZmnv5T45epl2hkJqBjUW5AmSFsXYRv1zeuR4tY9@us-east.connect.psdb.cloud/api-alma?ssl={"rejectUnauthorized":true}');
 
 export { pool };
