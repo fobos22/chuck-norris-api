74ff637cbae9f33d6be6a01a47d3b1a5b370882c
seiyadev <axelchavezuwu@gmail.com>
2022-11-19 18:27:57 -0600

    third-commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import { createPool } from "mysql2/promise";
 
-const pool = createPool('mysql://uloeu5msem1i842csl33:pscale_pw_B0dXZmnv5T45epl2hkJqBjUW5AmSFsXYRv1zeuR4tY9@us-east.connect.psdb.cloud/api-alma?ssl={"rejectUnauthorized":true}');
+const pool = createPool(process.env.DATABASE_URL);
 
 export { pool };
--- pages/api/getPhrase.js
+++ pages/api/getPhrase.js
@@ -3,8 +3,7 @@ import { pool } from "../../config/db";
 
 export default async function getPhrase(req, res) {
   const phrase = await axios("https://api.chucknorris.io/jokes/random");
-  await pool.query(
-    `INSERT INTO Phrases(phrase) VALUES("${phrase.data.value}")`
-  );
-  return res.status(204).json({ phrases: phrase.data.value });
+  const finalPhrase = `${phrase.data.value}`;
+  await pool.query(`INSERT INTO Phrases(phrase) VALUES("${finalPhrase}")`);
+  return res.status(204).json();
 }
